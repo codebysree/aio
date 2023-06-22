19ab473fbe8ee858a720110657976f00554f20da
Sreekumar <skaycse@gmail.com>
2023-06-22 20:53:05 +0530

    Initial Commit

---
+++ src/app/bmi/page.js
@@ -0,0 +1,79 @@
+'use client'
+
+import { useState } from "react"
+
+export default function bmi() {
+
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+
+
+  const handleHeightChange = (event) => {
+    setHeight(event.target.value)
+  }
+
+  const handleWeightChange = (event) => {
+    setWeight(event.target.value)
+  }
+
+  const calculateBMI = () => {
+    const bmiInt = weight / Math.pow(height, 2);
+    console.log(bmiInt)
+    switch (true) {
+      case bmiInt <= 18.5:
+        alert("Underweight");
+        break;
+      case bmiInt > 18.5 && bmiInt <= 24.9:
+        alert("Healthy Weight");
+        break;
+      case bmiInt >= 25.0 && bmiInt <= 29.9:
+        alert("Overweight");
+        break;
+      case bmiInt >= 30.0:
+        alert("Obesity");
+        break;
+      default:
+        alert("Error");
+    }
+
+  }
+
+
+  return (
+    <>
+      <form >
+        <div className="offset-3 row mt-1 g-3 align-items-center">
+          <div className="col-auto">
+            <label htmlFor="Height" className="col-form-label">Height</label>
+          </div>
+          <div className="col-4">
+            <input type="number" id="height" max={3} value={height} onChange={handleHeightChange}
+              name="height" className="form-control" aria-labelledby="HeightHelpIn" />
+          </div>
+          <div className="col-auto">
+            <span id="HeightHelpIn" className="form-text">
+              Height Must be meters (m).
+            </span>
+          </div>
+        </div>
+        <div className="offset-3 row mt-1 g-3 align-items-center">
+          <div className="col-auto">
+            <label htmlFor="Weight" className="col-form-label">Weight</label>
+          </div>
+          <div className="col-4">
+            <input type="number" id="weight" max={3} name="weight" value={weight} onChange={handleWeightChange}
+              className="form-control" aria-labelledby="WeightHelpIn" />
+          </div>
+          <div className="col-auto">
+            <span id="WeightHelpIn" className="form-text">
+              Weight Must be Kilogram (Kg).
+            </span>
+          </div>
+        </div>
+        <div className="col-6 d-grid mt-3 mx-auto">
+          <button className="btn btn-primary" type="button" onClick={calculateBMI} >Calculate My BMI</button>
+        </div>
+      </form>
+    </>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,46 @@
-import Image from 'next/image'
+import Link from 'next/link'
 import styles from './page.module.css'
 
-export default function Home() {
+const Cards = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.flexContainer}>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">BMI</h5>
+          <p className="card-text">BMI is a number calculated from weight and height to determine body weight categories..</p>
+          <Link href="/bmi" className="btn btn-primary">Go somewhere</Link>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Special title treatment</h5>
+          <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Special title treatment</h5>
+          <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
       </div>
-    </main>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Special title treatment</h5>
+          <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Special title treatment</h5>
+          <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
   )
 }
+
+export default Cards;
